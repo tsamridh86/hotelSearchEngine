278e83fc090d69cd687e5ec872c8502f2e0d0f73
tsamridh86 <t.samridh.86@gmail.com>
2017-04-03 19:47:44 +0530

    completed the intial base

--- booking.php
+++ booking.php
@@ -1,3 +1,12 @@
 <?php
-	echo "lol";
+	require 'config/config.php';
+	$conn = new Connector();
+	if( isset($_POST['userId']) && isset($_POST['hotelId']))
+	{
+		$query = "insert into bookings (userId, hotelId) values (".$_POST['userId']." , ".$_POST['hotelId'].")";
+		$conn->executeQuery($query);
+		echo "Successfully Booked Your Hotel!";
+	}
+	else 
+		echo "UserId or HotelId not found!";
 ?>
